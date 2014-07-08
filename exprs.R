9535a9b51334bbf56d37cf51113a37b7fdb90037
Alexey Sergushichev <alsergbox@gmail.com>
2014-07-08 13:31:45 +0400

    make eSet from files

---
+++ exprs.R
@@ -0,0 +1,21 @@
+# exprs.file <- "./work/data/mmp_artyomov_1195/mmu.mp.full.refseq.htcnt"
+# pdata.file <- "./work/data/mmp_artyomov_1195/conditions.tsv"
+
+makeExpressionSetFromFile <- function(
+    exprs.file, 
+    pdata.file
+    ) {
+    
+    require(Biobase)
+    exprs <- read.table(exprs.file)
+    exprs <- as.matrix(exprs)
+    pdata <- read.table(pdata.file, header=T, row.names=1)
+    pdata <- pdata[colnames(exprs), , drop=F]
+    meta <- data.frame(labelDescription = colnames(pdata))
+    rownames(meta) <- colnames(pdata)
+    
+    pdata <- new("AnnotatedDataFrame", data=pdata, varMeta=meta)
+    
+    eSet <- ExpressionSet(exprs, phenoData=pdata)
+    eSet
+}
