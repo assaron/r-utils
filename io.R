ea6f93c407e09fd3ec733cc1b4cf9e02ba588519
Alexey Sergushichev <alsergbox@gmail.com>
2014-07-08 13:32:59 +0400

    read.tsv and better write.tsv

--- io.R
+++ io.R
@@ -1,4 +1,20 @@
-write.tsv <- function(table, dir, ...) {
-  name <- deparse(substitute(table))
-  write.table(table, file.path(dir, paste0(name, ".tsv")), quote=F, row.names=T, col.names=NA, sep="\t")
+options(stringsAsFactors=F)
+
+read.tsv <- function(file, header=T, sep="\t", quote="", ...) {
+    read.table(file, header=header, sep=sep, quote=quote, ...)     
+}
+
+write.tsv <- function(table, dir, row.names=NA, col.names=NA, ...) {
+    name <- deparse(substitute(table))
+    table <- as.data.frame(table) 
+
+    if (is.na(row.names)) {
+        row.names <- is.character(attr(table, "row.names"))
+    }
+
+    if (!row.names && is.na(col.names)) {
+        col.names=T
+    }
+    write.table(table, file.path(dir, paste0(name, ".tsv")), quote=F,
+                row.names=row.names, col.names=col.names, sep="\t")
 }
